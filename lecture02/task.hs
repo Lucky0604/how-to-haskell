84cfd4fc6096650b27921dd4bcd138c125be7703
Lucky <175917645@qq.com>
2016-09-05 23:58:30 +0800

    basic of type and type-class

---
+++ lecture02/task.hs
@@ -0,0 +1,9 @@
+-- check the type about the value
+['a', 'b', 'c']
+-- [Char]
+('a', 'b', 'c')
+-- (Char, Char, Char)
+[(False, '0'), (True, '1')]
+-- [(Bool, Char)]
+([False, True], ['0', '1'])
+[tail, init, reverse]
