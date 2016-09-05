bd8d0b5418cc31ea8e57e12b624cd060c5f2b63e
Lucky <175917645@qq.com>
2016-09-06 00:07:54 +0800

    type of function

--- lecture02/task.hs
+++ lecture02/task.hs
@@ -1,9 +1,26 @@
 -- check the type about the value
 ['a', 'b', 'c']
--- [Char]
+-- ['a', 'b', 'c'] :: [Char]
 ('a', 'b', 'c')
--- (Char, Char, Char)
+-- ('a', 'b', 'c') :: (Char, Char, Char)
 [(False, '0'), (True, '1')]
--- [(Bool, Char)]
+-- [(False, '0'), (True, '1')] :: [(Bool, Char)]
 ([False, True], ['0', '1'])
+-- ([False, True], ['0', '1']) :: ([Bool], [Char])
 [tail, init, reverse]
+-- [tail, init, reverse] :: [[a] -> [a]]
+
+
+-- check the type about the function
+second xs = head (tail xs)
+-- second :: [a] -> a
+swap (x, y) = (y, x)
+-- swap :: (t1, t) -> (t, t1)
+pair x y = (x, y)
+-- pair :: t -> t1 -> (t, t1)
+double x = x * 2
+-- double :: Num a => a -> a
+palindrome xs = reverse xs == xs
+-- palindrome :: Eq a => [a] -> Bool
+twice f x = f (f x)
+-- twice :: (t -> t) -> t -> t
