632f1953026d48a5f21607dee2e78d2b4096471a
Lucky <175917645@qq.com>
2016-10-20 12:53:08 +0800

    add basic polymorphic type and function

--- lecture03/basic.hs
+++ lecture03/basic.hs
@@ -50,4 +50,37 @@ keepOnlyEven (Cons x xs)
   | even x = Cons x (keepOnlyEven xs)
   | otherwise = keepOnlyEven xs
 
+-- | ------------------------------------------------------------------
 
+-- | Polymorphism
+
+-- | polymorphic data type
+-- | E for Empty, C for Cons
+data List t = E | C t (List t)
+-- | t is a type variable
+lst1 :: List Int
+lst1 = C 3 (C 5 ( C 2 E))
+
+lst2 :: List Char
+lst2 = C 'x' (C 'y' (C 'z' E))
+
+lst3 :: List Bool
+lst3 = C True (C False E)
+
+-- | generalize filterIntList to work over the new polymorphic Lists
+-- | replace Empty by E and Cons by C:
+filterList _ E = E
+filterList p (C x xs)
+  | p x = C x (filterList p xs)
+  | otherwise = filterList p xs
+
+-- | mapList
+-- | general
+mapList :: (t -> t) -> List t -> List t
+-- | it means that when applying mapList, we always get a list with the same type of elements as the list we started with.
+
+-- | better
+mapList :: (a -> b) -> List a -> List b
+mapList _ E = E
+mapList f (C x xs) = C (f x) (mapList f xs)
+-- | polymorphic functions is that the caller gets to pick the types. When you write a polymorphic function, it must work for every possible input type
