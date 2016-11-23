0138676140c349df8ea631c5fb07dba25ad71129
Lucky <175917645@qq.com>
2016-11-23 18:26:57 +0800

    some instances about Applicative Functors

--- lecture10/Basic.hs
+++ lecture10/Basic.hs
@@ -36,3 +36,54 @@ one of the applicative laws
 -- | <$>, which is just fmap as an infix operator
 (<$>) :: (Functor f) => (a -> b) -> f a -> f b
 f <$> x = fmap f x
+
+
+-- | Lists (actually the list type constructor []) are applicative functors
+instance Applicative [] where
+  pure x = [x]
+  fs <*> xs = [f x | f <- fs, x <- xs]
+
+
+-- | another instance of Applicative is `IO`
+instance Applicative IO where
+  pure = return
+  a <*> b = do
+    f <- a
+    x <- b
+    return (f x)
+
+{-
+<*> were specialized for IO it would have a type of (<*>) :: IO (a -> b) -> IO a -> IO b
+
+It would take an I/O action that yields a function as its result and another I/O action
+and create a new I/O action from those two that, when performed,
+first performs the first one to get the function and then performs the
+second one to get the value and then it would yield that function applied to the value as its result.
+We used do syntax to implement it here.
+-}
+myAction :: IO String
+myAction = do
+  a <- getLine
+  b <- getLine
+  return $ a ++ b
+
+-- | Another way of writing this would be to use the applicative style.
+myAction' :: IO String
+myAction' = (++) <$> getLine <*> getLine
+
+{-
+The type of the expression (++) <$> getLine <*> getLine is IO String,
+which means that this expression is a completely normal I/O action like any other,
+which also holds a result value inside it, just like other I/O actions
+-}
+main = do
+  a <- (++) <$> getLine <*> getLine
+  putStrLn $ "The two lines concatenated turn out to be : " ++ a
+
+
+-- | another instance of `Applicative` is `(->) r`
+instance Applicative ((->) r) where
+  pure x = (\_ -> x)
+  f <*> g = \x -> f x (g x)
+
+
