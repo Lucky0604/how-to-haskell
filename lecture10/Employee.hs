398a6adc3290b5607a607c88a02a10b5e4af0d31
Lucky <175917645@qq.com>
2016-11-26 19:07:14 +0800

    applicative demo

--- lecture10/Employee.hs
+++ lecture10/Employee.hs
@@ -7,3 +7,24 @@ data Employee = Employee { name :: Name
 
 -- | the Employee constructor has type
 -- | Employee :: Name -> String -> Employee
+
+import Control.Applicative
+
+instance Applicative Maybe where
+  pure = Just
+  Nothing <*> _ = Nothing
+  _ <*> Nothing = Nothing
+  Just f <*> Just x = Just (f x)
+
+m_name1, m_name2 :: Maybe Name
+m_name1 = Nothing
+m_name2 = Just "Brent"
+
+m_phone1, m_phone2 :: Maybe String
+m_phone1 = Nothing
+m_phone2 = Just "555-1234"
+
+ex01 = Employee <$> m_name1 <*> m_phone1
+ex02 = Employee <$> m_name1 <*> m_phone2
+ex03 = Employee <$> m_name2 <*> m_phone1
+ex04 = Employee <$> m_name2 <*> m_phone2
